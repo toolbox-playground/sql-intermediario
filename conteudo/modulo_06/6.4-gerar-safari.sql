8e8f6a0de22888362b5abfaf31f87a7f464e5a48
Thiago Silva <163438232+thidevops@users.noreply.github.com>
2026-09-16 17:57:32 -0300

    Create 6.4-gerar-safari.sql

---
+++ conteudo/modulo_06/6.4-gerar-safari.sql
@@ -0,0 +1,13 @@
+--- Como Executar e Validar
+-- Para executar a procedure e popular a tabela com os 200 registros:
+
+```SQL
+CALL sp_gerar_log_safari_aleatorio();
+```
+
+-- Para visualizar os registros inseridos:
+
+```SQL
+SELECT * FROM safari_log;
+```
+
