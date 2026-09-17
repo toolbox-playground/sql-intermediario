b4085e35a2e5d67bb3527cdc8137ac7450aa4a22
thi-tbx <thiago.silva@tbxtech.com>
2026-09-17 13:43:01 -0300

    Update 7.7-trigger-update-delete.sql

--- conteudo/modulo_07/7.7-trigger-update-delete.sql
+++ conteudo/modulo_07/7.7-trigger-update-delete.sql
@@ -1,5 +1,6 @@
 --1. Trigger de UPDATE (Captura de Pokémon)
 --Disparada quando a quantidade de um Pokémon diminui, registrando a captura na eventos_safari:
+DROP TRIGGER IF EXISTS trg_log_captura_safari_update;
 
 DELIMITER //
 
@@ -7,18 +8,30 @@ CREATE TRIGGER `trg_log_captura_safari_update`
 AFTER UPDATE ON `safari_log`
 FOR EACH ROW
 BEGIN
+    DECLARE v_nome_pokemon VARCHAR(100);
+    DECLARE v_mensagem VARCHAR(255);
+
+    -- Busca o nome do pokemon a partir do pokedex_number inserido na safari_log
+    SELECT name INTO v_nome_pokemon 
+    FROM pokemon 
+    WHERE pokedex_number = NEW.pokedex_number;
+
+    -- Concatena a mensagem informando qual pokemon foi gerado
+    SET v_mensagem = CONCAT('O pokemon ', COALESCE(v_nome_pokemon, 'desconhecido'), '. Um ou mais pokemons foram capturados na regiao!');
     -- Verifica se a quantidade de pokémons diminuiu
     IF NEW.quantidade < OLD.quantidade THEN
         INSERT INTO eventos_safari (
             treinador_id,
             cidade,
             quantidade,
-            mensagem
+            mensagem,
+            pokedex_number
         ) VALUES (
             NULL,
             NEW.cidade,
             (OLD.quantidade - NEW.quantidade),
-            'Um ou mais pokemons foram capturados na regiao!'
+            v_mensagem,
+            NEW.pokedex_number
         );
     END IF;
 END //
@@ -27,6 +40,7 @@ DELIMITER ;
 
 -- 2. Trigger de DELETE (Catástrofe/Extermínio)
 -- Disparada quando um registro de habitat é removido por completo do log do Safari:
+DROP TRIGGER IF EXISTS trg_log_catastrofe_safari_delete;
 
 DELIMITER //
 
@@ -34,16 +48,30 @@ CREATE TRIGGER `trg_log_catastrofe_safari_delete`
 AFTER DELETE ON `safari_log`
 FOR EACH ROW
 BEGIN
+
+    DECLARE v_nome_pokemon VARCHAR(100);
+    DECLARE v_mensagem VARCHAR(255);
+
+    -- Busca o nome do pokemon a partir do pokedex_number inserido na safari_log
+    SELECT name INTO v_nome_pokemon 
+    FROM pokemon 
+    WHERE pokedex_number = OLD.pokedex_number;
+
+    -- Concatena a mensagem informando qual pokemon foi gerado
+    SET v_mensagem = CONCAT('ALERTA: Uma catastrofe aconteceu e os pokemons ', COALESCE(v_nome_pokemon, 'desconhecido'), '. foram exterminados da regiao! Autoridades foram acionadas para investigar.');
+    
     INSERT INTO eventos_safari (
         treinador_id,
         cidade,
         quantidade,
-        mensagem
+        mensagem,
+        pokedex_number
     ) VALUES (
         NULL,
         OLD.cidade,
         OLD.quantidade,
-        'ALERTA: Uma catastrofe aconteceu e os pokemons foram exterminados da regiao! Autoridades foram acionadas para investigar.'
+        v_mensagem,
+        OLD.pokedex_number
     );
 END //
 
