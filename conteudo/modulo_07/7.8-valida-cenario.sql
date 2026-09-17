54eb86538e007bb6f29e4a8ee784b3152969c713
thi-tbx <thiago.silva@tbxtech.com>
2026-09-17 13:53:06 -0300

    Update 7.8-valida-cenario.sql

--- conteudo/modulo_07/7.8-valida-cenario.sql
+++ conteudo/modulo_07/7.8-valida-cenario.sql
@@ -2,11 +2,10 @@
 -- 1. Simular uma captura (UPDATE):
 UPDATE safari_log 
 SET quantidade = quantidade - 1 
-WHERE pokedex_number = 25 AND cidade = 'Kanto Safari Zone';
+WHERE pokedex_number = 25 AND cidade = 'Fuchsia City';
 
 -- 2. Simular uma catástrofe (DELETE):
 DELETE FROM safari_log 
-WHERE pokedex_number = 25 AND cidade = 'Kanto Safari Zone';
-
+WHERE log_id = 789;
 -- 3. Verificar as notificações geradas:
-SELECT * FROM eventos_safari ORDER BY data_evento
+SELECT * FROM eventos_safari ORDER BY evento_id DESC;
